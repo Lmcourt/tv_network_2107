c98b2be07770a1bd36bac69de3d429e0f61e9074
Laura Court <laura.mcourt@gmail.com>
2021-07-21 12:27:53 -0400

    Add total salary, highest paid actor, and actors array

--- lib/show.rb
+++ lib/show.rb
@@ -1,1 +1,22 @@
+class Show
 
+  attr_reader :name, :creator, :characters, :total_salary
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+    @total_salary = 0
+  end
+
+  def total_salary
+    characters.map {|character| character.salary}.sum
+  end
+
+  def highest_paid_actor
+    characters.find { |character| character.actor if character.salary == 1_600_000 }
+  end
+
+  def actors
+    characters.map { |character| character.actor }
+  end
+end
